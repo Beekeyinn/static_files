522d49a479e895872e020cadc25e8169db423e5e
Bikin Shrestha <razzshrz07@gmail.com>
2023-08-07 14:16:00 +0545

    changed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
-
+import "./assets/App.css";
+import  entangle from "./assets/images/entangle.png" ;
+import hello from "./assets/images/hello_ai_blog.png"
 function App() {
   return (
     <div className="App">
-      This is an Empty Project that is used to host different static images or files for other projects.
+      <div>
+        <div>
+          <img src={entangle} alt="entangle" />
+        </div>
+        <div>
+          <img src={hello} alt="hello" />
+        </div>
+      </div>
+      This is an Empty Project that is used to host different static images or
+      files for other projects.
     </div>
   );
 }
