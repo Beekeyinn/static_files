c15a783f8af3b4cad6dc756cdb3ec0fedfccfcee
Bikin Shrestha <bikin@qodemedia.net>
2024-08-12 17:50:14 +0545

    'main' -> image added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./assets/App.css";
 import entangle from "./assets/images/entangle.png";
 import hello from "./assets/images/hello_ai_blog.png";
 import review from "./assets/images/review.png";
+import logo from  "./assets/images/logo.jpeg";
+import lock from  "./assets/images/lock.jpeg";
 function App() {
   return (
     <div className="App">
@@ -18,6 +20,12 @@ function App() {
         </div>
         <div>
           <img src={review} alt="review" />
+        </div>
+        <div>
+          <img src={lock} alt="review" />
+        </div>
+        <div>
+          <img src={logo} alt="review" />
         </div>
       </div>
       This is an Empty Project that is used to host different static images or
